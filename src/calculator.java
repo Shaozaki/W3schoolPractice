57ca0cc9ecedf83958ec1723d36fed4fa852a451
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-26 21:18:37 +0700

    up new

---
+++ src/calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        char operator;
+        Double number1, number2, result;
+
+        // Membuat Scanner class
+        Scanner input = new Scanner(System.in);
+        // minta pengguna untuk milih operasi
+        System.out.println("Pilih Operasi : +, -, *, atau / ");
+        operator = input.next().charAt(0);
+        
+        // minta paengguna untuk memasukan nomer
+        System.out.println("Masukan Angka Pertama : ");
+        number1 = input.nextDouble();
+
+        System.out.println("Masukan Angka Kedua : ");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+            case '+': 
+                    result = number1 + number2; 
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
