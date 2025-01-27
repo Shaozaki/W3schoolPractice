a16ef1cde395ba81f91211f89c2a6195effda760
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-01-27 19:20:10 +0700

    Lets refreshing

--- src/MultidimensionalArray.java
+++ src/MultidimensionalArray.java
@@ -1,6 +1,23 @@
 public class MultidimensionalArray {
-    public static void main(String[] args){
-        //Array Multideminsi
-        
+    public static void main(String[] args) {
+        // Array Multidemennsi
+        int[][] nomorku = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+        // System.out.println(nomorku[1][2]);
+
+        // nomorku [1][2] = 9; //mengubah values array
+        // System.out.println(nomorku[1][2]);
+
+        // perulangan for
+        for (int i = 0; i < nomorku.length; i++) {
+            for (int j = 0; j < nomorku[i].length; j++) {
+                System.out.print(nomorku[i][j]);
+            }
+        }
+        // perulangan for each
+        for (int[] row : nomorku) {
+            for (int i : row) {
+                System.out.print(i);
+            }
+        }
     }
 }
