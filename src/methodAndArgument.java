954efaa286a60dee2e9f494dc36861bec3012c67
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-02-01 11:03:31 +0700

    method Argument

---
+++ src/methodAndArgument.java
@@ -0,0 +1,9 @@
+public class methodAndArgument {
+ static void metodeku(String BackName){
+    System.out.print(BackName+" Ryuga");
+ }    
+public static void main(String[] args) {
+    metodeku("Zazkia");
+}
+
+}
