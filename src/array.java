b65d96ceac1683b6303e3658a577414f8ae6610b
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-02-06 15:53:49 +0700

    typo fixed

--- src/array.java
+++ src/array.java
@@ -1,5 +1,5 @@
 public class array {
-    //Array adalh sebuah kumpulan data yang dimulai dari 0S
+    //Array adalah sebuah kumpulan data yang dimulai dari 0 
     public static void main(String[] args){
         String[]  cars ={"BMW","TOYOTA","TESLA","HONDA"};
         cars[0]= "Opel";
