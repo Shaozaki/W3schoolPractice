fb103bac47b67bc464e29812798b91fb756f0b1a
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-02-14 22:30:58 +0700

    wkwk

--- src/MethodScope.java
+++ src/MethodScope.java
@@ -4,32 +4,32 @@
  
  */
 public class MethodScope {
- public static void main(String[] args) {
-    // kode disini tidak bisa memakai variabel variabel X
-    
-    int X = 100;
+    public static void main(String[] args) {
+        // kode disini tidak bisa memakai variabel variabel X
 
-    // kode disini bisa memakai variabel X
+        int X = 100;
 
+        // kode disini bisa memakai variabel X
 
-    System.out.println(X);
-    /*
-    Block Scope merupakan batas penggunaan variabel yang diantara kurung kurawal {}
-    block scope biasanya digunakan pada Statement if, while, for dll.
-     */
-
-     //kode disini tidak bisa memakai variabel s
-
-    {// merupakan block
+        System.out.println(X);
+        /*
+         * Block Scope merupakan batas penggunaan variabel yang diantara kurung kurawal
+         * {}
+         * block scope biasanya digunakan pada Statement if, while, for dll.
+         */
 
         // kode disini tidak bisa memakai variabel s
-        int s = 200;
 
-        // kode disini bisa memkai variabel s
-        System.out.println(s);
+        {// merupakan block
+
+            // kode disini tidak bisa memakai variabel s
+            int s = 200;
+
+            // kode disini bisa memkai variabel s
+            System.out.println(s);
+        }
+
+        // kode disini tidak bisa memakai variabel s
+
     }
-
-    // kode disini tidak bisa memakai variabel s
-
- }
 }
