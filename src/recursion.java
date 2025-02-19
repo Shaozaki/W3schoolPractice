894494da0611e95edec4fb6242f7244a5c372782
Shaozaki <fatihdaffauldzaki@gmail.com>
2025-02-19 15:25:57 +0700

    add some comment

--- src/recursion.java
+++ src/recursion.java
@@ -6,7 +6,6 @@ public class recursion {
     // contoh recursion
     public static void main(String[] args) {
         int result = sum(10);
-        
         System.out.println(result);
     }
 
@@ -16,7 +15,19 @@ public class recursion {
         } else {
             return 0;
         }
-    } 
-    /* ketika fungsi sum() dipanggil, maka akan menambahkan parameter 'k' ke sum dari seluruh 
-    bilangan yang lebih kecil dari 'k' dan  */
+    }
+    /*
+     * ketika fungsi sum() dipanggil, maka akan menambahkan parameter 'k' ke sum
+     * dari seluruh bilangan yang lebih kecil dari 'k' dan return ke result.
+     * ketika program akan mengikuti tahap berikut :
+     * 10 + sum(9)
+     * 10 + ( 9 + sum(8) )
+     * 10 + ( 9 + ( 8 + sum(7) ) )
+     * ...
+     * 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + sum(0)
+     * 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 + 0
+     * fungsi berhenti memanggil dirinya sendiri sejak 'k' adalah 0, program berhenti disitu dan returns result.
+     */
+
+     
 }
